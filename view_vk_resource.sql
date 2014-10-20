77ceff63699a558a319c5f9ed0126b546da430fb
Guillaume <guillaume.roger@spilgames.com>
2014-10-20 11:02:41 +0200

    Fix free/used mixup in view_vk_resources disk space

--- view_vk_resource.sql
+++ view_vk_resource.sql
@@ -3,8 +3,8 @@ CREATE OR REPLACE VIEW public.view_vk_resource AS
 -- Disk Space Utilization
   (SELECT 'Disk Space Utilization' AS Resource, 
           host_name AS Node, 
-          CAST( ROUND( disk_space_free_mb / 1024, 2 ) AS FLOAT(3) ) AS 'Used (GB)', 
-          CAST( ROUND( disk_space_used_mb / 1024, 2 ) AS FLOAT(3) ) AS 'Free (GB)', 
+          CAST( ROUND( disk_space_used_mb / 1024, 2 ) AS FLOAT(3) ) AS 'Used (GB)', 
+          CAST( ROUND( disk_space_free_mb / 1024, 2 ) AS FLOAT(3) ) AS 'Free (GB)', 
           CAST( ROUND( disk_space_total_mb / 1024, 2 ) AS FLOAT(3) ) AS 'Total (GB)' 
    FROM   v_monitor.host_resources 
    ORDER  BY host_name) 
